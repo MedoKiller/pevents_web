85349c52a08b52551d86b54846eea89c9b3d4eb1
Papi <dinomed123@gmail.com>
2023-12-05 22:35:45 +0100

    init working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Event } from './event';
+import { EventService } from './event.service';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [EventService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pevents_web';
+
+  public events: Event[] = [];
+
+  constructor(private eventService: EventService){}
+
+  ngOnInit(): void {
+    this.getEvents();  
+  }
+
+  public getEvents(): void {
+    this.eventService.getEvents().subscribe(
+      (response: Event[]) => {
+        this.events = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
 }
---
+++ src/app/event.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Event } from "./event";
+import { environment } from "../environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EventService{
+    private apiServerUrl=environment.apiBaseUrl;
+
+    constructor(private http: HttpClient){}
+
+    public getEvents(): Observable<Event[]>{
+        return this.http.get<Event[]>(`${this.apiServerUrl}/event/all`);
+    }
+
+    public addEvent(event: Event): Observable<Event>{
+        return this.http.post<Event>(`${this.apiServerUrl}/event/add`, event);
+    }
+
+    public updateEvent(event: Event): Observable<Event>{
+        return this.http.put<Event>(`${this.apiServerUrl}/event/update`, event);
+    }
+
+    public deleteEvent(eventId: number): Observable<void>{
+        return this.http.delete<void>(`${this.apiServerUrl}/event/delete/${eventId}`);
+    }
+
+}
